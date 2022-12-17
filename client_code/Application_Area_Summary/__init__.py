411051e0a1a4042a430c910b7c4b867acb3f0d2b
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2022-12-17 10:01:05 +0000

    Created Application_Area_Summary, Application_Area_Summary.RowTemplate3_copy, RowTemplate4 and Application_Area_Summary.RowTemplate3; Edited settings

---
+++ client_code/Application_Area_Summary/__init__.py
@@ -0,0 +1,14 @@
+from ._anvil_designer import Application_Area_SummaryTemplate
+from anvil import *
+import anvil.server
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class Application_Area_Summary(Application_Area_SummaryTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+    self.repeating_panel_1.items = app_tables.application_area.search(tables.order_by('application_area'))
+    # Any code you write here will run before the form opens.
+    
