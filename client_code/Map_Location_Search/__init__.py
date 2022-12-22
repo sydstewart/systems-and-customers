ad51501e591bca6b463626fbc00b85ac09d189a1
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2022-12-22 10:23:51 +0000

    Edited Map_Location_Search

--- client_code/Map_Location_Search/__init__.py
+++ client_code/Map_Location_Search/__init__.py
@@ -15,28 +15,37 @@ class Map_Location_Search(Map_Location_SearchTemplate):
     # Any code you write here will run before the form opens.
     self.search_name.text = 'London'
     self.location_name.text = 'London'
-
+    self.latitude  = self.longitude = None
+    
   def enable_search_btn(self, **event_args):
     """This method is called when the button is clicked"""
     self.search_btn.enabled = self.search_name.text != "" and self.location_name.text != ""
     pass
 
   def search_for_location(self, **event_args):
-#     results = GoogleMap.geocode(address=self.search_name.text)
+    results = GoogleMap.geocode(address=self.search_name.text)
 #     print(results)
-#     result = results[0].geometry.location
+    result = results[0].geometry.location
 #     print(result)
+    marker = GoogleMap.Marker(position=result)
+    self.map.add_component(marker)
+    self.map.center = result
+    self.map.zoom = 11
+    self.map.visible = True
+    self.add_location_btn.enabled = True
+    self.latitude = result.lat(marker)
+    self.longitude = result.lng(result)
 #       results = GoogleMap.geocode(address="Cambridge, UK")
-      map = GoogleMap()
+#     map = GoogleMap()
 #       self.map_1.
-      map.center = GoogleMap.LatLng(52.2053, 0.1218)
-      map.zoom = 13
-      marker = GoogleMap.Marker(
-        animation=GoogleMap.Animation.DROP,
-        position=GoogleMap.LatLng(52.2053, 0.1218)
-      )
-      
-      map.add_component(marker)
+#     map.center = GoogleMap.LatLng(52.2053, 0.1218)
+#     map.zoom = 13
+#     marker = GoogleMap.Marker(
+#       animation=GoogleMap.Animation.DROP,
+#       position=GoogleMap.LatLng(52.2053, 0.1218)
+#     )
+    
+#     map.add_component(marker)
          
 
 
