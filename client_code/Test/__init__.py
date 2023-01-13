53e115d06f40d0b086425d9be5e6fdaa3466c8fd
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-01-13 16:11:21 +0000

    Edited Test and settings

--- client_code/Test/__init__.py
+++ client_code/Test/__init__.py
@@ -12,37 +12,45 @@ class Test(TestTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    if self.text_box_1.text:
-       namesearch  = '%' + self.text_box_1.text + '%'
-       searchclause = 'Name= q.like(namesearch)'
-    else:
-        searchclause = ''
-    results ={}  
-    accounts = app_tables.suppported_products.search()
-    for row in accounts:
-         if row['Account'] in searchclause:
-            results.append(row)
-      
-      
+    kwargs = { 'employed': 'unemployed', 'age' : '77'}
+    results = app_tables.test.search(**kwargs)
+#     namevalue =  '%' + 'Alaska' + '%' # 'Alaska Native Medical Center - AC System'
+#     versionvalue = '%'+'CF 8'+'%'
+#     kwargs = {'Name': namevalue}
+#     print(kwargs)
+#     results = app_tables.suppported_products.search(q.all_of(**kwargs)) #q.like(**kwargs))
     self.repeating_panel_1.items = results
-    # Any code you write here will run before the form opens.
+    pass 
+#     if self.text_box_1.text:
+#        namesearch  = '%' + self.text_box_1.text + '%'
+#        searchclause = 'Name= q.like(namesearch)'
+#     else:
+#         searchclause = ''
+#     results ={}  
+#     accounts = app_tables.suppported_products.search()
+#     for row in accounts:
+#          if row['Account'] in searchclause:
+#             results.append(row)
+      
+      
+#     self.repeating_panel_1.items = results
+#     # Any code you write here will run before the form opens.
     
 
-  def text_box_1_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
+#   def text_box_1_pressed_enter(self, **event_args):
+#     """This method is called when the user presses Enter in this text box"""
     
-    if self.text_box_1.text:
-       namesearch  = '%' + self.text_box_1.text + '%'
-       searchclause = 'Name= q.like(namesearch)'
-    else:
-        searchclause = ''
-    results ={}  
-    accounts = app_tables.suppported_products.search()
-    for row in accounts:
-         if row['Account'] in namesearch:
-            results.append(row)
+#     if self.text_box_1.text:
+#        namesearch  = '%' + self.text_box_1.text + '%'
+#        searchclause = 'Name= q.like(namesearch)'
+#     else:
+#         searchclause = ''
+#     results ={}  
+#     accounts = app_tables.suppported_products.search()
+#     for row in accounts:
+#          if row['Account'] in namesearch:
+#             results.append(row)
       
       
-    self.repeating_panel_1.items = results
-    pass
+
 
