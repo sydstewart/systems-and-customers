9fc21192144a4fa51f5c7b49601984db8628fe51
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-01-14 16:20:34 +0000

    Edited Test and Searches.Use_kwargs

--- client_code/Searches/Use_kwargs.py
+++ client_code/Searches/Use_kwargs.py
@@ -21,6 +21,10 @@ def search_using_kwargs(self):
     search4 = self.apparea_drop_down.selected_value
     search5 = self.version_level_drop_down.selected_value
     search6 = self.NOT_interface_chkbox.checked
+    search7 = self.app_multi_select_drop_down.selected
+    search8 = self.region_dropdown.selected_value
+    search9 = self.customer_type_dropdown.selected_value
+    
     kwargs ={}
 
     if search1:
@@ -39,13 +43,15 @@ def search_using_kwargs(self):
     if search6 == True and search3:
          selectedinterface = ('%' + search3['Interface_Type'] + '%')
          kwargs['Interfaces'] = q.not_(q.like('%'+ selectedinterface + '%'))
-
+    if search7:
+      self.apparea_drop_down.selected_value = None
+      kwargs['CFApplicationArea']=q.any_of(*search7)
+    if search8:
+      kwargs['Location_c'] = search8
+    if search9:
+      kwargs['Customer_Type'] = search9
              
     results = app_tables.suppported_products.search(**kwargs)
-#     namevalue =  '%' + 'Alaska' + '%' # 'Alaska Native Medical Center - AC System'
-#     versionvalue = '%'+'CF 8'+'%'
-#     kwargs = {'Name': namevalue}
-#     print(kwargs)
-#     results = app_tables.suppported_products.search(q.all_of(**kwargs)) #q.like(**kwargs))
+
     self.repeating_panel_1.items = results
     self.text_box_1.text = len(results)
--- client_code/Test/__init__.py
+++ client_code/Test/__init__.py
@@ -23,13 +23,20 @@ class Test(TestTemplate):
     self.in_use_drop_down.items = inusestatus
     self.version_level_drop_down.items = version_level
     self.interfaces_drop_down.items = [(str(row['Interface_Type']), row) for row in app_tables.interface_types.search(tables.order_by('Interface_Type'))]
-
+    self.app_multi_select_drop_down.items = applications
+    self.region_dropdown.items = regions
+    self.customer_type_dropdown.items = customertype
+    
     
     search1 = self.in_use_drop_down.selected_value
     search2 = None    #Name
-    search3 = self.in_use_drop_down.selected_value
+    search3 = self.interfaces_drop_down.selected_value
     search4 = self.apparea_drop_down.selected_value
     search5 = self.version_level_drop_down.selected_value
+    search6 = self.NOT_interface_chkbox.checked
+    search7 = self.app_multi_select_drop_down.selected
+    search8 = self.region_dropdown.selected_value
+    
     kwargs ={}
 
     if search1:
@@ -77,6 +84,24 @@ class Test(TestTemplate):
     """This method is called when this checkbox is checked or unchecked"""
     search_using_kwargs(self)
     pass
+
+  def app_multi_select_drop_down_change(self, **event_args):
+    """This method is called when the selected values change"""
+    search_using_kwargs(self)
+    pass
+
+  def region_dropdown_change(self, **event_args):
+    """This method is called when an item is selected"""
+    search_using_kwargs(self)
+    pass
+
+  def customer_type_dropdown_change(self, **event_args):
+    """This method is called when an item is selected"""
+    search_using_kwargs(self)
+    pass
+
+
+
 
 
 
