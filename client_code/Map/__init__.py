762ed1dbca805968427676a45f4c4596be05563b
sydney.w.stewart@gmail.com <sydney.w.stewart@gmail.com>
2023-01-20 12:11:56 +0000

    20012023 1211

--- client_code/Map/__init__.py
+++ client_code/Map/__init__.py
@@ -8,8 +8,8 @@ import anvil.server
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-# from ..Searches.apparea_search import apparea_search 
-# from ..Searches.multi_search import multi_search 
+from ..Searches.apparea_search import apparea_search 
+from ..Searches.multi_search import multi_search 
 
 class Map(MapTemplate):
   def __init__(self, **properties):
@@ -57,7 +57,7 @@ class Map(MapTemplate):
     selecttedinusestatus = self.In_Use_Status_dropdown.selected_value
 #     print(selectedapparea['application_area'])
 #     selectedapparea  = ('%' + selectedapparea['application_area'] + '%')
-#     apparea_search(self, selectedapparea,selecttedinusestatus)
+    apparea_search(self, selectedapparea,selecttedinusestatus)
       
       
   def In_Use_Status_dropdown_change(self, **event_args):
---
+++ client_code/Searches/multi_search.py
@@ -0,0 +1,44 @@
+from anvil import *
+import anvil.users
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+# This is a module.
+# You can define variables and functions here, and use them from any form. For example, in a top-level form:
+#
+#    from . import Module1
+#
+#    Module1.say_hello()
+#
+
+
+def multi_search(self, selecttedinusestatus2, selectedapps):
+    if selecttedinusestatus2 and selectedapps  :
+          locations= app_tables.suppported_products.search(CFApplicationArea=q.any_of(*selectedapps), InUseStatus=selecttedinusestatus2)  # anvil.server.call('get_all_locations_with_two',selectedapps, selecttedinusestatus2 )
+      
+    elif  not selecttedinusestatus2 and selectedapps  :
+          locations = app_tables.suppported_products.search(CFApplicationArea=q.any_of(*selectedapps)) #anvil.server.call('get_all_locations_with_selectedapps_alone', selectedapps)
+#       
+    elif  selecttedinusestatus2 and not selectedapps  :
+          locations = app_tables.suppported_products.search(InUseStatus=selecttedinusestatus2) #anvil.server.call('get_all_locations_with_In_Use_alone', selecttedinusestatus2)
+#       
+    elif  not selecttedinusestatus2 and not selectedapps: 
+          locations = app_tables.suppported_products.search()  # anvil.server.call('get_all_locations')
+    
+    if locations:  
+          self.hits_textbox.text = len(locations)
+          for location in locations:
+            position = GoogleMap.LatLng(location['latitude'], location['longitude'])
+            marker = GoogleMap.Marker(position=position)
+            self.map.add_component(marker)
+          
+            marker.add_event_handler("click", self.marker_click)
+            self.markers[marker] = location['Name'] + ' ' + location['InUseStatus'] + ' ' + location['Live_version_no']
+            
+#           print( 'got db entries')  
+          def marker_click(self, sender, **event_args):
+            i = GoogleMap.InfoWindow(content=Label(text=self.markers[sender]))
+            i.open(self.map, sender)  
